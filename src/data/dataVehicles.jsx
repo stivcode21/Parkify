dfc3ed6f66a815062b3552e7504b881cf5749d86
stivcode21 <stivenelcodificador@gmail.com>
2025-07-06 21:19:44 -0500

    feat: vehicles data aggregated by moment

---
+++ src/data/dataVehicles.jsx
@@ -0,0 +1,12 @@
+export const vehicles = [
+  { placa: "OTW57H", tipo: "Moto", fecha: "25/06/25", hora: "12:26" },
+  { placa: "ABC123", tipo: "Carro", fecha: "26/06/25", hora: "14:30" },
+  { placa: "XYZ789", tipo: "Moto", fecha: "27/06/25", hora: "09:15" },
+  { placa: "LMN456", tipo: "Carro", fecha: "28/06/25", hora: "11:45" },
+  { placa: "QRS321", tipo: "Moto", fecha: "29/06/25", hora: "16:00" },
+  { placa: "TUV654", tipo: "Carro", fecha: "30/06/25", hora: "08:20" },
+  { placa: "JKL987", tipo: "Moto", fecha: "01/07/25", hora: "10:10" },
+  { placa: "PQR852", tipo: "Carro", fecha: "02/07/25", hora: "13:55" },
+  { placa: "STU963", tipo: "Moto", fecha: "03/07/25", hora: "15:30" },
+  { placa: "VWX741", tipo: "Carro", fecha: "04/07/25", hora: "17:45" },
+];
