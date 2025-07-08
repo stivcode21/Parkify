4d5231682d00b9da8d176bb902140f9e731bc1b0
stivcode21 <stivenelcodificador@gmail.com>
2025-07-07 23:32:46 -0500

    refactor: new hook integrated into the component Reloj

--- src/components/molecules/reloj/Reloj.jsx
+++ src/components/molecules/reloj/Reloj.jsx
@@ -1,31 +1,10 @@
-import { useEffect, useState } from "react";
 import styles from "./Reloj.module.css";
+import useHoraColombia from "@/hooks/useHoraColombia";
 
 const Reloj = () => {
-  const [hora, setHora] = useState("");
+  const horaActual = useHoraColombia();
 
-  useEffect(() => {
-    const actualizarHora = () => {
-      const ahora = new Date();
-
-      const opciones = {
-        hour: "2-digit",
-        minute: "2-digit",
-        hour12: false,
-        timeZone: "America/Bogota",
-      };
-
-      const horaColombia = ahora.toLocaleTimeString("es-CO", opciones);
-      setHora(horaColombia);
-    };
-
-    actualizarHora();
-    const intervalo = setInterval(actualizarHora, 60000); // actualizar cada minuto
-
-    return () => clearInterval(intervalo);
-  }, []);
-
-  return <p className={styles.hour}>{hora}</p>;
+  return <p className={styles.hour}>{horaActual}</p>;
 };
 
 export default Reloj;
